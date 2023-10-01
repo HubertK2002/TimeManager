2099b04c7c57ac2b7223e0cb9e9b2e8c63235843
Hustak <hubistef16@wp.pl>
2023-10-01 18:08:02 +0200

    Obsłużenie poziomych gestów za pomocą PanResponder w tygodniowych aktywnościach

--- Screens/WeekActivities.js
+++ Screens/WeekActivities.js
@@ -1,14 +1,48 @@
-import React, { useRef, useEffect } from 'react';
-import { SafeAreaView, ScrollView } from 'react-native';
+import React, { useRef, useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView ,StyleSheet,  Animated, PanResponder, View} from 'react-native';
 import {TimeBlock, generateTimeBlocks} from '../Views/TimeBlock'; 
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation} from '@react-navigation/native';
 import { getDay, format  } from 'date-fns';
 import { pl } from 'date-fns/locale';
+import { subDays } from 'date-fns';
+
+const handleSwipeLeft = () => {
+    const newDate = subDays(currentDate, 1);
+    console.log("Test");
+    setCurrentDate(newDate);
+};
+
+const handleSwipeRight = () => {
+    const newDate = subDays(currentDate, -1);
+    setCurrentDate(newDate);
+};
+
 
 const WeekActivities = () => {
-    const now = new Date();
-    const formattedDate = format(now, 'yyyy-MM-dd');
-    const day = format(now, 'EEEE', { locale: pl });;
+    const [isDragging, setIsDragging] = useState(false);
+    const pan = useRef(new Animated.ValueXY()).current;
+    const panResponder = PanResponder.create({
+        onMoveShouldSetPanResponder: (evt, gestureState) => {
+          const dx = Math.abs(gestureState.dx);
+          const dy = Math.abs(gestureState.dy);
+          
+          if (dx > dy && dx > 10) { // Dodatkowy warunek, aby uniknąć fałszywego wyzwalania
+            setIsDragging(true);
+            console.log("gest");
+            return true;
+          }
+          return false;
+        },
+        onPanResponderRelease: () => {
+          setIsDragging(false);
+        },
+        onPanResponderTerminate: () => {
+          setIsDragging(false);
+        },
+      });
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const formattedDate = format(currentDate, 'yyyy-MM-dd');
+    const day = format(currentDate, 'EEEE', { locale: pl });;
     const { blocks, currentIndex } = generateTimeBlocks();
     const scrollViewRef = useRef(null);
     const navigation = useNavigation();
@@ -25,7 +59,7 @@ const WeekActivities = () => {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView ref={scrollViewRef}>
+      <ScrollView ref={scrollViewRef} scrollEnabled={!isDragging}  {...panResponder.panHandlers} horizontal={false}>
         {blocks.map(time => (
           <TimeBlock key={time} time={time} />
         ))}
@@ -33,5 +67,13 @@ const WeekActivities = () => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+    box: {
+      width: 100,
+      height: 100,
+      backgroundColor: 'red',
+    },
+  });
 
 export default WeekActivities;
